33b43ee94058087dfbf274ef49fbbbecb28016f2
Yitzi Ginzberg <emesdg@gmail.com>
2020-05-07 18:40:22 +0300

    title removed

--- api/src/main/java/org/yitzi/video/api/Uploader.java
+++ api/src/main/java/org/yitzi/video/api/Uploader.java
@@ -9,10 +9,9 @@ import java.io.File;
 
 public class Uploader {
 
-    public void upload(String apiKey, String title, String tag, String pathName) throws ResponseException {
+    public void upload(String apiKey, String tag, String pathName) throws ResponseException {
         Client client = new ClientFactory().create(apiKey);
         VideoInput videoInput = new VideoInput();
-        videoInput.title = title;
         videoInput.tags.add(tag);
         client.videos.upload(new File(pathName), videoInput);
     }
--- api/src/test/java/org/yitzi/video/api/apivideo/UploaderTest.java
+++ api/src/test/java/org/yitzi/video/api/apivideo/UploaderTest.java
@@ -8,6 +8,6 @@ class UploaderTest {
 
     @Test
     void upload() throws ResponseException {
-        new Uploader().upload("XevyeUNNDV66E67QmTk23hOwryfdejOHI5U8M1tHXND", "My Second Test Title", "test tag 1", "src/test/resources/My Video 1.mp4");
+        new Uploader().upload("XevyeUNNDV66E67QmTk23hOwryfdejOHI5U8M1tHXND", "test tag 3", "src/test/resources/My Video 1.mp4");
     }
 }
